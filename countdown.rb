053d786af8f96e764a0277c9b4b00d4e832e5dc4
Jonathan McKnight <themcknightrider@gmail.com>
2020-06-02 14:43:35 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
-#write your code here
-
-def countdown
+def countdown(seconds)
+  max_num = seconds
+  while seconds.between?(1, max_num)
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1 
+  end
+  "HAPPY NEW YEAR!"
 end
+countdown(10)
+
+def countdown_with_sleep(seconds)
+  max_num = seconds
+  while seconds.between?(1, max_num)
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+    sleep 1 
+  end
+  "HAPPY NEW YEAR!"
+end 
