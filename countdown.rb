553b3779c11692593a1afe88ee7f1689904c2404
Jonathan McKnight <themcknightrider@gmail.com>
2020-06-02 14:40:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -12,7 +12,8 @@ def countdown_with_sleep(seconds)
   max_num = seconds
   while seconds.between?(1, max_num)
     puts "#{seconds} SECOND(S)!"
-    seconds -= 1 
+    seconds -= 1
+    sleep 1 
   end
   "HAPPY NEW YEAR!"
 end 
